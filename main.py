2dd2b7a5cf1602c9b5d2dbd8a8f5d11cdad6649d
josefpokorny <pepek.pok@seznam.cz>
2019-04-26 18:02:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 
 print("Hello world from python")
+
+
+
+from keboola import docker
+
+
+
+# Configuration parameters
+cfg_keboola = docker.Config("/data/")
+with open("/data/config.json", mode="r") as config_file:
+    cfg = json.load(config_file)
+
+params = cfg["parameters"]
+image = cfg["image_parameters"]
+
+
+USERNAME = params["hovno"]
+
+print(USERNAME)
